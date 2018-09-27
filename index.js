ae115b896170a173adfeca6265ccddd888556cd1
citation0097 <32308475+citation0097@users.noreply.github.com>
2018-09-27 12:30:33 -0700

    initial setup

---
+++ index.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const PORT = process.env.PORT || 8000;
+
+const app = express();
+
+app.get('/',(req, res)=>{
+    res.send('<h1>This is my awesome </h1>');
+});
+
+app.listen(PORT, () => {
+    console.log('Server Running on Port' + PORT);
+});
+
